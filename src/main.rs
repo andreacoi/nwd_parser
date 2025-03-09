bebe180cbc2d4c491ab6072188882f10c1d76e02
Andrea Coi <andrea.coi@outlook.it>
2025-03-09 12:09:55 +0100

    create the entry point of the applications manages the user input and the output of the app.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use nwd_parser::domparser::parse_html;
+use nwd_parser::nwd_pdf::gen_nwdpdf;
+use nwd_parser::help::help;
+use std::env;
+use std::path::PathBuf;
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+     if args.len() != 3 {
+        help();
+    } else {
+        let path: PathBuf = env::current_dir()?;
+        let complete_path: String = format!("{}/{}", path.display(), &args[2]);
+        match &args[1].as_str() {
+            &"-f" => {
+                let nwd_data = parse_html(complete_path);
+                let filename: String = args[2].clone().replace(".html", "");
+                gen_nwdpdf(filename, nwd_data);
+            },
+            _ => help(),
+        }
+    }
+
+    Ok(())
 }
